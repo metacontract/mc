5e925ba1c38668c7e93ed7898874e196bb41df9e
Kai Hiroi <kai.hiroi@pm.me>
2023-11-30 00:25:49 +0900

    update: predicate msgsender

--- src/predicates/MsgSender.sol
+++ src/predicates/MsgSender.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-// import {console2} from "forge-std/console2.sol";
 import {StorageLib} from "../StorageLib.sol";
 
 library MsgSender {
     error NotOwner();
     function shouldBeOwner() external view {
-        if (msg.sender != StorageLib.getOwnerStorage().owner) revert NotOwner();
+        if (msg.sender != StorageLib.$Owner().owner) revert NotOwner();
     }
 }
