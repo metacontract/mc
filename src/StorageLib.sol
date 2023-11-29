45f6bcc9255f41f4acb7f12cf89d4248170f46ac
Kai Hiroi <kai.hiroi@pm.me>
2023-11-30 00:24:08 +0900

    update: StorageLib

--- src/StorageLib.sol
+++ src/StorageLib.sol
@@ -5,15 +5,13 @@ library StorageLib {
     /*********************
         OwnerStorage
      ********************/
-    /// @custom:storage-location erc7201:example.owner
+    /// @custom:storage-location erc7201:ucs.operation.Owner
     struct OwnerStorage {
         address owner;
     }
     bytes32 internal constant OWNER_STORAGE_LOCATION = 0xf14ccab36e70fe6703d70047fd9f791010c6456c7ac5d1945a4518f360bd1fe3;
 
-    function getOwnerStorage() internal pure returns (OwnerStorage storage $) {
-        assembly {
-            $.slot := OWNER_STORAGE_LOCATION
-        }
+    function $Owner() internal pure returns (OwnerStorage storage $) {
+        assembly { $.slot := OWNER_STORAGE_LOCATION }
     }
 }
