816fc6dfce9f4cacbadff3b88ac6886f30390aa9
KaiHiroi <kai.hiroi@pm.me>
2023-12-10 02:02:17 +0530

    update: CloneOp

--- src/ops/CloneOp.sol
+++ src/ops/CloneOp.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import {ERC7546Clones} from "@ucs-contracts/src/ERC7546Clones.sol";
+// import {ERC7546Clones} from "@ucs-contracts/src/ERC7546Clones.sol";
+import {ERC7546Proxy} from "@ucs-contracts/src/proxy/ERC7546Proxy.sol";
 import {ERC7546Utils} from "@ucs-contracts/src/proxy/ERC7546Utils.sol";
 
 import {ICloneOp} from "../interfaces/ops/ICloneOp.sol";
@@ -10,7 +11,8 @@ contract CloneOp is ICloneOp {
     /// DO NOT USE STORAGE DIRECTLY !!!
 
     function clone(bytes calldata initData) external returns (address proxy) {
-        proxy = ERC7546Clones.clone(ERC7546Utils.getDictionary(), initData);
+        // proxy = ERC7546Clones.clone(ERC7546Utils.getDictionary(), initData);
+        proxy = address(new ERC7546Proxy(ERC7546Utils.getDictionary(), initData));
         emit ProxyCloned(proxy);
     }
 
