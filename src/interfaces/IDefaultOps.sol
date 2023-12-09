c339b94ca26976b28f61a6cd17bcb4b1a46e05f5
KaiHiroi <kai.hiroi@pm.me>
2023-12-09 23:27:01 +0530

    update default ops interface

--- src/interfaces/IDefaultOps.sol
+++ src/interfaces/IDefaultOps.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.22;
 import {ICloneOp} from "./ops/ICloneOp.sol";
 import {IInitSetAdminOp} from "./ops/IInitSetAdminOp.sol";
 import {ISetImplementationOp} from "./ops/ISetImplementationOp.sol";
+import {ERC7546ProxyEvents} from "@ucs-contracts/src/proxy/ERC7546ProxyEvents.sol";
 
 interface IDefaultOps is
     ICloneOp,
     IInitSetAdminOp,
-    ISetImplementationOp
+    ISetImplementationOp,
+    ERC7546ProxyEvents
 {}
