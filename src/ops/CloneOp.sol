b1a8b2275dc847d6fabe7994cee4ce2fbdb11534
Kai Hiroi <kai.hiroi@pm.me>
2024-02-06 02:40:27 +0900

    update: CloneOp etherscan

--- src/ops/CloneOp.sol
+++ src/ops/CloneOp.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.22;
 
 // import {ERC7546Clones} from "@ucs-contracts/src/ERC7546Clones.sol";
-import {ERC7546Proxy} from "@ucs-contracts/src/proxy/ERC7546Proxy.sol";
+import {ERC7546ProxyEtherscan} from "@ucs-contracts/src/proxy/ERC7546ProxyEtherscan.sol";
 import {ERC7546Utils} from "@ucs-contracts/src/proxy/ERC7546Utils.sol";
 
 import {ICloneOp} from "../interfaces/ops/ICloneOp.sol";
@@ -12,7 +12,7 @@ contract CloneOp is ICloneOp {
 
     function clone(bytes calldata initData) external returns (address proxy) {
         // proxy = ERC7546Clones.clone(ERC7546Utils.getDictionary(), initData);
-        proxy = address(new ERC7546Proxy(ERC7546Utils.getDictionary(), initData));
+        proxy = address(new ERC7546ProxyEtherscan(ERC7546Utils.getDictionary(), initData));
         emit ProxyCloned(proxy);
     }
 
