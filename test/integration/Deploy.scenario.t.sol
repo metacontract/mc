3e7ca5639e11d7fe88a9bc4d7dd7a15485ab027e
Shogo Ochiai <shogo.ochiai@protonmail.com>
2024-03-01 17:49:12 +0900

    Revert "Devkit"

--- test/integration/Deploy.scenario.t.sol
+++ test/integration/Deploy.scenario.t.sol
@@ -2,22 +2,12 @@
 pragma solidity ^0.8.24;
 
 import {console2} from "forge-std/console2.sol";
-import {UCSTest} from "../../utils/UCSTest.sol";
-import {CloneOp} from "src/ops/CloneOp.sol";
+import {UCSTestBase} from "../../utils/UCSTestBase.sol";
 
-contract DeployScenarioTest is UCSTest {
-    function setUp() public startPrankWithDeployer {
-        address proxy = ucs .deploy("MyProxy")
-                            .set(ucs.ops.stdOps.clone)
-                            .getProxy().toAddress();
-        CloneOp(proxy).clone("");
-        // address proxy = ucs .findDictionary("Default")
-        //                     .deployProxy("MyProxy");
-        // address proxy = ucs .deployDictionary("Default")
-        //                     .deployProxy("MyProxy");
-    }
+contract DeployScenarioTest is UCSTestBase {
+    function setUp() public startPrankWithDeployer {}
 
     function test_Success_DeployNewProxy() public {
-        // address proxy = newProxy();
+        address proxy = newProxy();
     }
 }
