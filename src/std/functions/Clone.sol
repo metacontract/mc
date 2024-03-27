1369ff14bdb16660617bcfdcf683858cad7088cf
Kai Hiroi <kai.hiroi@pm.me>
2024-03-27 13:35:46 +0900

    rm IClone

--- src/std/functions/Clone.sol
+++ src/std/functions/Clone.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import {IClone} from "../interfaces/functions/IClone.sol";
-
 import {ERC7546ProxyEtherscan} from "@ucs-contracts/src/proxy/ERC7546ProxyEtherscan.sol";
 import {ERC7546Utils} from "@ucs-contracts/src/proxy/ERC7546Utils.sol";
 
@@ -12,8 +10,9 @@ import {ERC7546Utils} from "@ucs-contracts/src/proxy/ERC7546Utils.sol";
     @custom:version 0.1.0
     @custom:schema v0.1.0
  */
-contract Clone is IClone {
+contract Clone {
     /// DO NOT USE STORAGE DIRECTLY !!!
+    event ProxyCloned(address proxy);
 
     function clone(bytes calldata initData) external returns (address proxy) {
         proxy = address(new ERC7546ProxyEtherscan(ERC7546Utils.getDictionary(), initData));
