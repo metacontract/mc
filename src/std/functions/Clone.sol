9555b812ad37ff06a2f7302d49a74d0c7bff85ea
Kai Hiroi <kai.hiroi@pm.me>
2024-03-27 10:22:45 +0900

    update clone

--- src/std/functions/Clone.sol
+++ src/std/functions/Clone.sol
@@ -1,23 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-/**
-    Standard Op:
-        >>> Clone <<<
-    Last Updated: v0.1.0
- */
+import {IClone} from "../interfaces/functions/IClone.sol";
 
-// import {ERC7546Clones} from "@ucs-contracts/src/ERC7546Clones.sol";
 import {ERC7546ProxyEtherscan} from "@ucs-contracts/src/proxy/ERC7546ProxyEtherscan.sol";
 import {ERC7546Utils} from "@ucs-contracts/src/proxy/ERC7546Utils.sol";
 
-import {IClone} from "../interfaces/functions/IClone.sol";
-
+/**
+    < MC Standard Function >
+    @title Clone
+    @custom:version 0.1.0
+    @custom:schema v0.1.0
+ */
 contract Clone is IClone {
     /// DO NOT USE STORAGE DIRECTLY !!!
 
     function clone(bytes calldata initData) external returns (address proxy) {
-        // proxy = ERC7546Clones.clone(ERC7546Utils.getDictionary(), initData);
         proxy = address(new ERC7546ProxyEtherscan(ERC7546Utils.getDictionary(), initData));
         emit ProxyCloned(proxy);
     }
