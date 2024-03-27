4b877e7488d00f7a6ab1b7d220c8ee895f3118a8
Kai Hiroi <kai.hiroi@pm.me>
2024-03-27 10:22:56 +0900

    update GetDeps

--- src/std/functions/GetDeps.sol
+++ src/std/functions/GetDeps.sol
@@ -1,33 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-// storage
-// import {Storage} from "../storage/Storage.sol";
 import {IGetDeps} from "../interfaces/functions/IGetDeps.sol";
+
 import {ERC7546Utils} from "@ucs-contracts/src/proxy/ERC7546Utils.sol";
 import {DictionaryBase} from "@ucs-contracts/src/dictionary/DictionaryBase.sol";
 
-// predicates
-// import {MsgSender} from "../predicates/MsgSender.sol";
-
+/**
+    < MC Standard Function >
+    @title GetDeps
+    @custom:version 0.1.0
+    @custom:schema v0.1.0
+ */
 contract GetDeps is IGetDeps {
     /// DO NOT USE STORAGE DIRECTLY !!!
 
-    modifier requires() {
-        _;
-    }
-
-    modifier intents() {
-        _;
-    }
-
-    function getDeps() external view requires intents returns(Op[] memory ops) {
-        DictionaryBase dictionary = DictionaryBase(ERC7546Utils.getDictionary());
+    function getDeps() external view returns(Op[] memory) {
+        DictionaryBase dictionary = DictionaryBase(ERC7546Utils.getDictionary()); // TODO IDictionary
         bytes4[] memory selectors = dictionary.supportsInterfaces();
-        ops = new Op[](selectors.length);
+        Op[] memory ops = new Op[](selectors.length);
         for (uint i; i < selectors.length; ++i) {
             ops[i].selector = selectors[i];
             ops[i].implementation = dictionary.getImplementation(selectors[i]);
         }
+        return ops;
     }
 }
