3c71c2364a21c2685bf74158734fb9dad34299f5
Haku <32897506+0xHaku@users.noreply.github.com>
2024-03-28 12:39:09 +0900

    add ERC7201Calc Script

---
+++ script/ERC7201Calc.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+
+contract ERC7201Calc is Script {
+    function run() public {
+        try vm.prompt("storage location") returns (string memory storage_location) {
+            run(storage_location);
+        } catch (bytes memory) {
+            console.log("error");
+        }
+    }
+
+    function run(string memory storage_location) public view {
+        bytes32 res = keccak256(abi.encode(uint256(keccak256(abi.encodePacked(storage_location))) - 1)) & ~bytes32(uint256(0xff));
+        console.logBytes32(res);
+    }
+}
