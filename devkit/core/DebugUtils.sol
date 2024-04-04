9ec8593895dc44ad8a803a8961e46bd4cbb00ebd
Kai Hiroi <kai.hiroi@pm.me>
2024-04-04 16:51:09 +0900

    Create DebugUtils

---
+++ devkit/core/DebugUtils.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {MCDevKit} from "devkit/MCDevKit.sol";
+
+// Debug
+import {Debug} from "devkit/debug/Debug.sol";
+import {Logger} from "devkit/debug/Logger.sol";
+
+using DebugUtils for MCDevKit;
+library DebugUtils {
+    string constant LIB_NAME = "MCDebug";
+
+    /**----------------
+        🐞 Debug
+    ------------------*/
+    function startDebug(MCDevKit storage mc) internal returns(MCDevKit storage) {
+        Debug.startDebug();
+        return mc;
+    }
+    function stopLog(MCDevKit storage mc) internal returns(MCDevKit storage) {
+        Debug.stopLog();
+        return mc;
+    }
+    function insert(MCDevKit storage mc, string memory message) internal returns(MCDevKit storage) {
+        Logger.insert(message);
+        return mc;
+    }
+
+    /**
+        Record Start
+     */
+    function recordExecStart(MCDevKit storage, string memory funcName, string memory params) internal returns(uint) {
+        return Debug.recordExecStart(LIB_NAME, funcName, params);
+    }
+    function recordExecStart(MCDevKit storage mc, string memory funcName) internal returns(uint) {
+        return mc.recordExecStart(funcName, "");
+    }
+
+    /**
+        Record Finish
+     */
+    function recordExecFinish(MCDevKit storage mc, uint pid) internal returns(MCDevKit storage) {
+        Debug.recordExecFinish(pid);
+        return mc;
+    }
+
+}
