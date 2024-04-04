09f21c1d274500960a52d84e9d11e2bf41df8d58
Kai Hiroi <kai.hiroi@pm.me>
2024-04-04 18:16:06 +0900

    Create Context Utils

---
+++ devkit/core/ContextUtils.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {MCDevKit} from "devkit/MCDevKit.sol";
+
+using ContextUtils for MCDevKit;
+library ContextUtils {
+    string constant LIB_NAME = "MCContext";
+
+    /**-----------------------------
+        ♻️ Reset Current Context
+    -------------------------------*/
+    function reset(MCDevKit storage mc) internal returns(MCDevKit storage) {
+        uint pid = mc.recordExecStart("reset");
+        mc.dictionary.reset();
+        mc.functions.reset();
+        mc.proxy.reset();
+        return mc.recordExecFinish(pid);
+    }
+
+}
