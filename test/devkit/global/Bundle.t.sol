c6bd4c85565a17e9893b556ea301b58360332b80
Kai Hiroi <kai.hiroi@pm.me>
2024-04-05 19:08:49 +0900

    Add MCBundleTest

--- test/devkit/global/Bundle.t.sol
+++ test/devkit/global/Bundle.t.sol
@@ -8,6 +8,9 @@ import {StringUtils} from "devkit/utils/StringUtils.sol";
 
 contract DevKitTest_MCBundle is MCDevKitTest {
 
+    /**---------------------------
+        🌱 Init Custom Bundle
+    -----------------------------*/
     function test_Success_init_withName() public {
         string memory name = "TestBundleName";
         mc.init(name);
@@ -16,4 +19,38 @@ contract DevKitTest_MCBundle is MCDevKitTest {
         assertTrue(mc.functions.currentBundleName.isEqual(name));
     }
 
+    // verify genUniqueBundleName
+    // function test_Success_init_withoutName() public {}
+
+    function test_Success_ensureInit_beforeInit() public {
+        string memory name = mc.functions.genUniqueBundleName();
+
+        mc.ensureInit();
+
+        assertTrue(mc.functions.bundles[name.safeCalcHash()].name.isEqual(name));
+        assertTrue(mc.functions.currentBundleName.isEqual(name));
+    }
+
+    function test_Success_ensureInit_afterInit() public {
+        string memory name = mc.functions.genUniqueBundleName();
+
+        mc.init();
+
+        assertTrue(mc.functions.bundles[name.safeCalcHash()].name.isEqual(name));
+        assertTrue(mc.functions.currentBundleName.isEqual(name));
+
+        string memory name2 = mc.functions.genUniqueBundleName();
+        mc.ensureInit();
+
+        assertTrue(mc.functions.bundles[name2.safeCalcHash()].name.isEmpty());
+        assertTrue(mc.functions.currentBundleName.isEqual(name));
+    }
+
+
+    /**---------------------
+        🔗 Use Function
+    -----------------------*/
+    /**------------------
+        🪟 Use Facade
+    --------------------*/
 }
