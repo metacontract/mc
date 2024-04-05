3edc82cb04c01fa83b2e3336bdc6b8c615648f45
Kai Hiroi <kai.hiroi@pm.me>
2024-04-05 19:05:25 +0900

    Update SetupLib

--- devkit/global/SetupLib.sol
+++ devkit/global/SetupLib.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.8.24;
 
 import {MCDevKit} from "devkit/MCDevKit.sol";
 
-using SetupLib for MCDevKit;
-library SetupLib {
-    string constant LIB_NAME = "MCSetup";
-
 /**********************************
     🏗 Setup
         🧩 Setup Standard Funcs
 ***********************************/
+library SetupLib {
+    using SetupLib for MCDevKit;
+    string constant LIB_NAME = "SetupLib";
+
+
+    /**----------------------------
+        🧩 Setup Standard Funcs
+    ------------------------------*/
     function setupStdFuncs(MCDevKit storage mc) internal returns(MCDevKit storage) {
-        uint pid = mc.recordExecStart("setupMCStdFuncs");
+        uint pid = mc.recordExecStart("setupStdFuncs");
         mc.functions.std.assignAndLoad()
                         .deployIfNotExists()
                         .configureStdBundle();
