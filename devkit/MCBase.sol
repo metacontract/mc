2dcf00991e2a233bf4b4a484e63367ae6794add7
Kai Hiroi <kai.hiroi@pm.me>
2024-04-06 10:02:45 +0900

    Update boilerplates

--- devkit/MCBase.sol
+++ devkit/MCBase.sol
@@ -1,19 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// 🛠 FORGE STD
 import {CommonBase} from "forge-std/Base.sol";
+import {Script as ForgeScript} from "forge-std/Script.sol";
+import {Test as ForgeTest} from "forge-std/Test.sol";
 
 import {MCDevKit} from "devkit/global/MCDevKit.sol";
+import {ForgeHelper} from "devkit/utils/ForgeHelper.sol";
 
-import {Config} from "./Config.sol";
 
 abstract contract MCBase is CommonBase {
     MCDevKit internal mc;
     uint256 internal deployerKey;
     address internal deployer;
+}
 
-    constructor() {
-        if (Config.DEBUG_MODE) mc.startDebug();
-        if (Config.SETUP_STD_FUNCS) mc.setupStdFuncs();
+abstract contract MCScriptBase is MCBase, ForgeScript {
+    modifier startBroadcastWith(string memory envKey) {
+        _startBroadcastWith(envKey);
+        _;
+    }
+
+    modifier startBroadcastWithDeployerPrivKey() {
+        _startBroadcastWith("DEPLOYER_PRIV_KEY");
+        _;
+    }
+
+    function _startBroadcastWith(string memory envKey) internal {
+        deployerKey = ForgeHelper.getPrivateKey("DEPLOYER_PRIV_KEY");
+        deployer = vm.addr(deployerKey);
+        vm.startBroadcast(deployerKey);
+    }
+}
+
+abstract contract MCTestBase is MCBase, ForgeTest {
+    modifier startPrankWith(string memory envKey) {
+        _startPrankWith(envKey);
+        _;
+    }
+    modifier startPrankWithDeployer() {
+        _startPrankWith("DEPLOYER");
+        _;
+    }
+    function _startPrankWith(string memory envKey) internal {
+        deployer = vm.envOr(envKey, makeAddr(envKey));
+        vm.startPrank(deployer);
+    }
+
+    modifier assumeAddressIsNotReserved(address addr) {
+        ForgeHelper.assumeAddressIsNotReserved(addr);
+        _;
     }
 }
--- devkit/MCScript.sol
+++ devkit/MCScript.sol
@@ -1,38 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {ForgeHelper} from "./utils/ForgeHelper.sol";
-import {Config} from "./Config.sol";
+import {Config} from "devkit/config/Config.sol";
 
 // 💬 ABOUT
 // Meta Contract's default Script based on Forge Std Script
 
-// 🛠 FORGE STD
-import {Script as ForgeScript} from "forge-std/Script.sol";
-
 // 📦 BOILERPLATE
-import {MCBase} from "./MCBase.sol";
+import {MCScriptBase} from "./MCBase.sol";
 
 // ⭐️ MC SCRIPT
-abstract contract MCScript is MCBase, ForgeScript {
+abstract contract MCScript is MCScriptBase {
     constructor() {
-        if (Config.DEBUG_MODE) mc.startDebug();
-        if (Config.USE_DEPLOYED_STD) mc.setupStdFuncs();
-    }
-
-    modifier startBroadcastWith(string memory envKey) {
-        _startBroadcastWith(envKey);
-        _;
-    }
-
-    modifier startBroadcastWithDeployerPrivKey() {
-        _startBroadcastWith("DEPLOYER_PRIV_KEY");
-        _;
-    }
-
-    function _startBroadcastWith(string memory envKey) internal {
-        deployerKey = ForgeHelper.getPrivateKey("DEPLOYER_PRIV_KEY");
-        deployer = vm.addr(deployerKey);
-        vm.startBroadcast(deployerKey);
+        Config().load();
+        if (Config().DEBUG_MODE) mc.startDebug();
+        if (Config().SETUP_STD_FUNCS) mc.setupStdFuncs();
     }
 }
