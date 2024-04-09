ee678d769c21d345da806cda1894a6d956ea087e
Kai Hiroi <kai.hiroi@pm.me>
2024-04-10 08:06:16 +0900

    Update BundleRegistry Lib

--- devkit/core/BundleRegistry.sol
+++ devkit/core/BundleRegistry.sol
@@ -1,23 +1,137 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+/**--------------------------
+    Apply Support Methods
+----------------------------*/
+import {ProcessLib} from "devkit/method/debug/ProcessLib.sol";
+    using ProcessLib for BundleRegistry global;
+import {Inspector} from "devkit/method/inspector/Inspector.sol";
+    using Inspector for BundleRegistry global;
+import {MappingAnalyzer} from "devkit/method/inspector/MappingAnalyzer.sol";
+    using MappingAnalyzer for mapping(string => Bundle);
 
+// Validation
+import {Valid, valid} from "devkit/error/Valid.sol";
 // Core Type
 import {Bundle} from "devkit/core/Bundle.sol";
-// Core Method
-import {BundleRegistryLib} from "devkit/method/core/BundleRegistryLib.sol";
-// Support Methods
-import {ProcessLib} from "devkit/method/debug/ProcessLib.sol";
-import {Inspector} from "devkit/method/inspector/Inspector.sol";
+import {Function} from "devkit/core/Function.sol";
 
 
 /**========================
     📙 Bundle Registry
 ==========================*/
+using BundleRegistryLib for BundleRegistry global;
 struct BundleRegistry {
     mapping(string name => Bundle) bundles;
     string currentBundleName;
 }
-using BundleRegistryLib for BundleRegistry global;
-//  Support Methods
-using ProcessLib for BundleRegistry global;
-using Inspector for BundleRegistry global;
+library BundleRegistryLib {
+    /**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        🌱 Init Bundle
+        ✨ Add Custom Function
+        🔏 Load and Assign Custom Function from Env
+        🧺 Add Custom Function to Bundle
+        🪟 Set Facade
+        🔼 Update Current Context Function & Bundle
+        🔍 Find Function & Bundle
+        🏷 Generate Unique Name
+        🔍 Find Custom Function
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    /**---------------------
+        🌱 Init Bundle
+    -----------------------*/
+    function init(BundleRegistry storage bundle, string memory name) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("init");
+        bundle.bundles[name].assignName(name);
+        bundle.safeUpdateCurrentBundle(name);
+        return bundle.finishProcess(pid);
+    }
+    function safeInit(BundleRegistry storage bundle, string memory name) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("safeInit");
+        Valid.isNotEmpty(name);
+        return bundle.assertBundleNotExists(name)
+                        .init(name)
+                        .finishProcess(pid);
+    }
+
+
+    /**-------------------------------------
+        🧺 Add Custom Function to Bundle
+    ---------------------------------------*/
+    function addToBundle(BundleRegistry storage bundle, Function storage functionInfo) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("addToBundle", "function");
+        bundle.findCurrentBundle().pushFunction(functionInfo);
+        return bundle.finishProcess(pid);
+    }
+    function addToBundle(BundleRegistry storage bundle, Function[] storage functions) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("addToBundle", "bundle"); // TODO params
+        bundle.findCurrentBundle().pushFunctions(functions);
+        return bundle.finishProcess(pid);
+    }
+
+
+    /**------------------
+        🪟 Set Facade
+    --------------------*/
+    function set(BundleRegistry storage bundle, string memory name, address facade) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("set");
+        bundle.bundles[name]
+                    .assertExists()
+                    .assignFacade(facade);
+        return bundle.finishProcess(pid);
+    }
+    function set(BundleRegistry storage bundle, address facade) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("set");
+        return bundle.set(bundle.findCurrentBundleName(), facade).finishProcess(pid);
+    }
+
+
+    /**------------------------------------------------
+        🔼 Update Current Context
+    --------------------------------------------------*/
+    function safeUpdateCurrentBundle(BundleRegistry storage bundle, string memory name) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("safeUpdateCurrentBundle");
+        Valid.isNotEmpty(name);
+        bundle.currentBundleName = name;
+        return bundle.finishProcess(pid);
+    }
+
+
+    /**-----------------------------------------------
+        ♻️ Reset Current Context Function & Bundle
+    -------------------------------------------------*/
+    function reset(BundleRegistry storage bundle) internal returns(BundleRegistry storage) {
+        uint pid = bundle.startProcess("reset");
+        delete bundle.currentBundleName;
+        return bundle.finishProcess(pid);
+    }
+
+
+    /**-------------------------------
+        🔍 Find Function & Bundle
+    ---------------------------------*/
+    function findBundle(BundleRegistry storage bundle, string memory name) internal returns(Bundle storage) {
+        uint pid = bundle.startProcess("findBundle");
+        return bundle.bundles[name].finishProcess(pid);
+    }
+    function findCurrentBundle(BundleRegistry storage bundle) internal returns(Bundle storage) {
+        uint pid = bundle.startProcess("findCurrentBundle");
+        return bundle.findBundle(bundle.findCurrentBundleName()).finishProcess(pid);
+    }
+        function findCurrentBundleName(BundleRegistry storage bundle) internal returns(string memory) {
+            uint pid = bundle.startProcess("findCurrentBundleName");
+            Valid.isNotEmpty(bundle.currentBundleName);
+            return bundle.currentBundleName;
+            // return bundle.currentBundleName.recordExecFinish(pid);
+        }
+
+
+    /**-----------------------------
+        🏷 Generate Unique Name
+    -------------------------------*/
+    function genUniqueName(BundleRegistry storage bundle) internal returns(string memory name) {
+        return bundle.bundles.genUniqueName();
+    }
+
+}
