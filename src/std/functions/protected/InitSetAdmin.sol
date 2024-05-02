8a3e45641797363bf3f422d3fe82097b547add27
Kai Hiroi <kai.hiroi@pm.me>
2024-05-02 12:01:15 +0900

    Add Test: InitSetAdmin

--- src/std/functions/protected/InitSetAdmin.sol
+++ src/std/functions/protected/InitSetAdmin.sol
@@ -7,14 +7,14 @@ import {Storage} from "../../storage/Storage.sol";
 // predicates
 import {ProtectionBase} from "./utils/ProtectionBase.sol";
 
-/**
-    < MC Standard Function >
-    @title InitSetAdmin
-    @custom:version 0.1.0
-    @custom:schema v0.1.0
+/** < MC Standard Function >
+ *  @title InitSetAdmin
+ *  @custom:version v0.1.0
+ *  @custom:schema v0.1.0
  */
 contract InitSetAdmin is ProtectionBase {
     /// DO NOT USE STORAGE DIRECTLY !!!
+
     event AdminSet(address admin);
 
     function initSetAdmin(address admin) external initializer {
