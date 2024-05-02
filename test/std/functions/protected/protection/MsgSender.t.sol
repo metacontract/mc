baad90c49baf988557b125ba6fb5cf64ddcb39f1
Kai Hiroi <kai.hiroi@pm.me>
2024-05-02 12:14:33 +0900

    Move & Rename MsgSender Test

--- test/std/functions/protected/protection/MsgSender.t.sol
+++ test/std/functions/protected/protection/MsgSender.t.sol
@@ -5,10 +5,10 @@ import {Test} from "forge-std/Test.sol";
 
 import {MsgSender} from "mc-std/functions/protected/protection/MsgSender.sol";
 
-contract MsgSenderSpecTest is Test {
+contract MsgSenderTest is Test {
     function setUp() public {}
 
-    function test_MsgSender_RevertIf_SenderIsNotAdmin() public {
+    function test_ShouldBeAdmin_RevertIf_SenderIsNotAdmin() public {
         vm.expectRevert(MsgSender.NotAdmin.selector);
         MsgSender.shouldBeAdmin();
     }
