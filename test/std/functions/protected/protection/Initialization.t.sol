412c21d57aa4893a1bcd044a5a9446b61e3e772b
Kai Hiroi <kai.hiroi@pm.me>
2024-05-02 12:22:05 +0900

    Add Initialization Test

---
+++ test/std/functions/protected/protection/Initialization.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+
+import {Initialization} from "mc-std/functions/protected/protection/Initialization.sol";
+import {Storage} from "mc-std/storage/Storage.sol";
+
+contract InitializationTest is Test {
+
+    function test_ShouldNotBeCompleted_Success() public {
+        Initialization.shouldNotBeCompleted();
+    }
+
+    function test_ShouldNotBeCompleted_RevertIf_VersionIsNotZero() public {
+        Storage.Initialization().initialized = 1;
+
+        vm.expectRevert(Initialization.InvalidInitialization.selector);
+        Initialization.shouldNotBeCompleted();
+    }
+
+    function test_WillBeCompleted_Success() public {
+        vm.expectEmit(address(this));
+        emit Initialization.Initialized(1);
+        Initialization.willBeCompleted();
+
+        assertEq(Storage.Initialization().initialized, 1);
+    }
+
+}
