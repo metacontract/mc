77f427603426e7962a05998076c2410c942aaea4
Kai Hiroi <kai.hiroi@pm.me>
2024-05-11 13:46:54 +0900

    Refactor MCFinderLib Test

--- test/devkit/global/MCFinderLib.t.sol
+++ test/devkit/global/MCFinderLib.t.sol
@@ -2,11 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {MCTestBase} from "devkit/MCBase.sol";
-import {MessageHead as HEAD} from "devkit/system/message/MessageHead.sol";
-import {Formatter} from "devkit/types/Formatter.sol";
-    using Formatter for string;
-import {DummyFunction} from "devkit/test/dummy/DummyFunction.sol";
-import {DummyFacade} from "devkit/test/dummy/DummyFacade.sol";
+import {MessageHead as REASON} from "devkit/system/message/MessageHead.sol";
+import {Dummy} from "devkit/test/dummy/Dummy.sol";
 
 contract MCFinderLibTest is MCTestBase {
 
@@ -14,18 +11,18 @@ contract MCFinderLibTest is MCTestBase {
         🏠 Find Current Proxy Address
     ------------------------------------*/
     function test_toProxyAddress_Success() public {
-        mc.init();
-        mc.use(DummyFunction.dummy.selector, address(new DummyFunction()));
-        mc.useFacade(address(new DummyFacade()));
+        bytes4 selector = Dummy.functionSelector();
+        mc.use(selector, Dummy.functionAddress());
+        mc.useFacade(Dummy.facadeAddress());
         mc.deploy();
 
         address proxy = mc.toProxyAddress();
 
-        (bool success,) = proxy.call(abi.encodeWithSelector(DummyFunction.dummy.selector));
+        (bool success,) = proxy.call(abi.encodeWithSelector(selector));
         assertTrue(success);
     }
     function test_toProxyAddress_Revert_NoCurrentProxy() public {
-        vm.expectRevert(HEAD.CURRENT_PROXY_NOT_EXIST.toBytes());
+        mc.expectRevert(REASON.CURRENT_PROXY_NOT_EXIST);
         mc.toProxyAddress();
     }
 
@@ -33,11 +30,10 @@ contract MCFinderLibTest is MCTestBase {
         📚 Find Current Dictionary Address
     ------------------------------------------*/
     function test_toDictionaryAddress_Success() public {
-        bytes4 selector = DummyFunction.dummy.selector;
-        address impl = address(new DummyFunction());
-        mc.init();
+        bytes4 selector = Dummy.functionSelector();
+        address impl = Dummy.functionAddress();
         mc.use(selector, impl);
-        mc.useFacade(address(new DummyFacade()));
+        mc.useFacade(Dummy.facadeAddress());
         mc.deploy();
 
         address dictionary = mc.toDictionaryAddress();
@@ -47,7 +43,7 @@ contract MCFinderLibTest is MCTestBase {
         assertEq(address(uint160(uint256(bytes32(ret)))), impl);
     }
     function test_toDictionaryAddress_Revert_NoCurrentProxy() public {
-        vm.expectRevert(HEAD.CURRENT_DICTIONARY_NOT_EXIST.toBytes());
+        mc.expectRevert(REASON.CURRENT_DICTIONARY_NOT_EXIST);
         mc.toDictionaryAddress();
     }
 
