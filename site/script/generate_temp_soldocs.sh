c8a5999ccb183d2640554783dbb9ae5c5410400e
Kai Hiroi <kai.hiroi@pm.me>
2024-10-30 23:31:05 +0900

    fix

--- site/script/generate_temp_soldocs.sh
+++ site/script/generate_temp_soldocs.sh
@@ -5,7 +5,7 @@ command -v forge >/dev/null 2>&1 || {
 	curl -L https://foundry.paradigm.xyz | bash
 
     # Update PATH to include Foundry binaries
-    # export PATH=$HOME/.foundry/bin:$PATH
+    export PATH=$HOME/.foundry/bin:$PATH
 
 	# Update Foundry
 	$HOME/.foundry/bin/foundryup
@@ -13,4 +13,12 @@ command -v forge >/dev/null 2>&1 || {
 
 # Navigate to the parent directory, set PATH, generate docs, and return to site directory
 cd ..
-$HOME/.foundry/bin/forge doc -o temp_soldocs/
+forge doc -o temp_soldocs/
+
+# Check if the file exists
+if [ -f "temp_soldcs/src/src/devkit/Flattened.sol/abstract.CommonBase.md" ]; then
+    # Output the contents of the file
+    cat temp_soldcs/src/src/devkit/Flattened.sol/abstract.CommonBase.md
+else
+    echo "File not found: temp_soldcs/src/src/devkit/Flattened.sol/abstract.CommonBase.md"
+fi
