e72a571dd9f6aa5b449ecb700931eb3fb3c7f615
Kai Hiroi <kai.hiroi@pm.me>
2024-10-31 01:09:42 +0900

    dump

--- site/script/remove_temp_soldocs.sh
+++ site/script/remove_temp_soldocs.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
 
+# Check if the file exists
+if [ -f "docs/03-api/03-api-details/Flattened.sol/abstract.CommonBase.md" ]; then
+    # Output the contents of the file
+    cat docs/03-api/03-api-details/Flattened.sol/abstract.CommonBase.md
+else
+    echo "File not found: docs/03-api/03-api-details/Flattened.sol/abstract.CommonBase.md"
+fi
+
 cd ..
+
+# Check if the file exists
+if [ -f "temp_soldocs/src/src/devkit/Flattened.sol/abstract.CommonBase.md" ]; then
+    # Output the contents of the file
+    cat temp_soldocs/src/src/devkit/Flattened.sol/abstract.CommonBase.md
+else
+    echo "File not found: temp_soldocs/src/src/devkit/Flattened.sol/abstract.CommonBase.md"
+fi
+
 rm -rf temp_soldocs
